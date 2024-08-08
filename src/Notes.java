943d34b12f49524106d349f0ba59c981d63b0e35
herreang000 <herreang000@warren.k12.in.us>
2024-08-08 11:15:23 -0400

    Final

--- src/Notes.java
+++ src/Notes.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Notes {
+public class Notes {
+    public static void main(String[] args) {
+        boolean isAlien = false;
+
+        if (isAlien == false) {
+            System.out.println("It is not an alien");
+            System.out.println("And I'm scared of aliens");
+        }
+
+        int topScore = 80;
+        if(topScore < 100){
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 81;
+        if(topScore > secondTopScore && topScore < 100){
+            System.out.println("Greater than second top score and less then 100");
+        }
+
+        if(topScore > 90 || secondTopScore <= 90){
+            System.out.println("Either or both of the conditions are true");
+        }
+
+    }
 }
